c59d47337840c292553fbb686965f814618f65ec
Cyro <bluecyro@protonmail.com>
2024-02-03 14:36:49 -0600

    Tidy up, explicit extensions to handle stuff in-world

--- Stage/StageHelper.cs
+++ Stage/StageHelper.cs
@@ -5,7 +5,10 @@ namespace Stagefright;
 
 public static class StageHelper
 {
-    public const string STAGE_PREFIX = "DMXUniverse:";
+    public const string STAGE_IDENTIFIER = "DMXUniverse";
+    public const string STAGE_PREFIX = STAGE_IDENTIFIER + ":";
+    public const string VARIABLE_SLOT_TAG = STAGE_IDENTIFIER + ".variable_slot";
+    public const string DMX_DEVICE_REFRESH_TAG = "DMXDevice.refresh";
 
 
 
@@ -25,7 +28,7 @@ public static class StageHelper
     {
         var world = temp.World;
         var all = world.AllSlots.ToList();
-
+        
         world.DestroyUniverses();
         all.ForEach(s => s.TrySetupStage());
 
@@ -51,6 +54,7 @@ public static class StageHelper
     {
         // Pre-emptively destroy an existing universe if it already exists
         World world = root.World;
+        root.DestroyStage();
         world.DestroyUniverse(info.Universe);
 
 
@@ -60,28 +64,31 @@ public static class StageHelper
         Universe uni = world.SetupUniverse(info.Count, info.Universe);
 
         // Find the variable space root
-        var space = root.FindSpace("DMXUniverse") ?? root.AttachComponent<DynamicVariableSpace>();
-        space.SpaceName.Value ??= "DMXUniverse";
+        var space = root.FindSpace(STAGE_IDENTIFIER) ?? root.AttachComponent<DynamicVariableSpace>();
+        space.SpaceName.Value ??= STAGE_IDENTIFIER;
         
         // Create a variables slot
-        Slot variableRoot = root.FindChildOrAdd("<b><i>variable<color=hero.purple>(</color>s<color=hero.purple>)</color></i></b>", false);
-        Slot streamSlot = variableRoot.FindChildOrAdd("DMX Stream Variables", false);
-        streamSlot.OrderOffset = 1024;
+        Slot variableRoot = root.FindChild(s => s.Tag == VARIABLE_SLOT_TAG);
 
-
-        // Blow up all old DMX variables in the space
-        for (int i = 0; i < 512; i++)
+        if (variableRoot == null)
         {
-            string variableName = (i + 1).ToString();
-            space.GetManager<IField<float>>(variableName, false)?.DeleteAllReadable();
+            Slot varRoot = root.AddSlot("<b><i>variable<color=hero.purple>(</color>s<color=hero.purple>)</color></i></b>", false);
+            varRoot.OrderOffset = 1024;
+            Slot vars = varRoot.AddSlot("DMX Stream Variables");
+            vars.Tag = VARIABLE_SLOT_TAG;
+            variableRoot = vars;
+        }
 
-            // Create a new variable in their place if we're within the new universe's stream count
+        for (int i = 0; i < uni.ChannelCount; i++)
+        {
+            // Create a new variable in their place
+            string variableName = (i + 1).ToString();
             if (i < uni.Streams.Length)
-                streamSlot.CreateVariable<IValue<float>>((i + 1).ToString(), uni.Streams[i], false); // DMX channels are 1-indexed. Ouch.
+                variableRoot.CreateVariable<IValue<float>>(variableName, uni.Streams[i], false); // DMX channels are 1-indexed. Ouch.
         }
 
 
-        ProtoFluxHelper.DynamicImpulseHandler.TriggerDynamicImpulse(root, "DMXDevice.refresh", false);
+        ProtoFluxHelper.DynamicImpulseHandler.TriggerDynamicImpulse(root, DMX_DEVICE_REFRESH_TAG, false);
     }
 
 
@@ -105,5 +112,43 @@ public static class StageHelper
             data = new(-1, -1);
             return false;
         }
+    }
+
+
+
+    public static void DestroyStage(this Slot slot)
+    {
+        if (slot.Name.TryParseUniverseInfo(out StageInfo data))
+        {
+            Stagefright.Msg($"Destroying stage {data.Universe} with {data.Count} channels in {slot.World.Name}");
+            Slot variableRoot = slot.FindChild(s => s.Tag == VARIABLE_SLOT_TAG);
+            
+            if (variableRoot != null)
+            {
+                var streamVars = variableRoot.GetComponents<DynamicReferenceVariable<IValue<float>>>();
+                for (int i = 0; i < 512; i++)
+                {
+                    streamVars.FirstOrDefault(c => c.VariableName.Value == (i + 1).ToString())?.Destroy();
+                }
+            }
+            slot.World.DestroyUniverse(data.Universe);
+        }
+    }
+
+
+
+    public static void DestroyWorldStages(this World world)
+    {
+        Stagefright.Msg($"Destroying all stages in {world.Name}!");
+        world.AllSlots.ToList().ForEach(s => s.DestroyStage());
+        world.DestroyUniverses();
+    }
+
+
+
+    public static void DestroyWorldStages(Slot temp)
+    {
+        temp.World.DestroyWorldStages();
+        temp.Destroy();
     }
 }
